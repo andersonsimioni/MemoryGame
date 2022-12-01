25b84e0599647aafcd6c8f35c67183889ba718d4
Anderson Simioni Assunçao <anderson.assuncao@parceiro.mrv.com.br>
2022-12-01 20:04:03 -0300

    card animation

--- assets/js/views/home/index.js
+++ assets/js/views/home/index.js
@@ -2,18 +2,25 @@ $(document).ready(()=>{
 
     function newGame(){
         var cardsNumber = prompt('Digite um número de pares de cartas entre 4 e 7');
+        if(cardsNumber == null) return;
         if(!parseInt(cardsNumber) || !(cardsNumber>=4 && cardsNumber<=7))
         {   
             alert('Por favor, preencha um valor correto!')
             return;
         }
-        console.log(`Gerando jogo com ${cardsNumber} pares de cartas..`);
+        console.log(`Gerando jogo com ${cardsNumber} pares de cartas..`);   
+    }
 
+    function cardClick(){
+        var el = $(this);
+        el.toggleClass('gamecard-hide');
+        el.toggleClass('gamecard-show');
         
     }
 
     function createEvents(){
         $('#btn-start-new-game').click(newGame);
+        $('.gamecard').click(cardClick);
     }
 
     function init(){
